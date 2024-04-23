c53ba3cf8fcc6f123e46a0f90db43841cf3c469f
SantiagoMuslera <santiagomuslera@gmail.com>
2024-04-23 10:29:36 -0300

    Creacion de la clase Aerolinea

--- src/sistemaAutogestion/Aerolinea.java
+++ src/sistemaAutogestion/Aerolinea.java
@@ -4,10 +4,60 @@
  */
 package sistemaAutogestion;
 
+import java.util.Objects;
+
 /**
  *
  * @author Santiago
  */
 public class Aerolinea {
+    private String nombre;
+    private String pais;
+    private int cantMaxAviones;
     
+    public Aerolinea(String nombre, String pais, int cantMaxAviones){
+        this.nombre = nombre;
+        this.pais = pais;
+        this.cantMaxAviones = cantMaxAviones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getCantMaxAviones() {
+        return cantMaxAviones;
+    }
+
+    public void setCantMaxAviones(int cantMaxAviones) {
+        this.cantMaxAviones = cantMaxAviones;
+    }
+    
+    @Override
+    public String toString() {
+        return "Aerolinea{" + "nombre=" + nombre + ", pais=" + pais + ", cantMaxAviones=" + cantMaxAviones + '}';
+    }
+    
+    @Override
+    public boolean equals(Object a) {
+        if (!this.getClass().equals(a.getClass())) {
+            return false;
+        } else {
+            Aerolinea aerolinea=(Aerolinea) a;
+            
+        return this.nombre==aerolinea.getNombre();
+        }
+    }
 }
